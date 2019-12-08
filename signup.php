20a5caf4ae8645b4cffed3785d2cd24698e8b99f
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2019-12-07 18:08:23 -0800

    Works pretty well now, yay

--- signup.php
+++ signup.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Sign up for this super cool fake site!!</title>
+		<title>Sign up!</title>
 	</head>
-	<body>
+	<h2 style="background-color:Gray; margin: 0px;">Become a member of this super cool, super fake site!!</h2>
+	<body style="background-color:LightGray;">
 		<form action="./process.php" method="POST">
 		<label>Username: </label><br/>
-		<input type="text" value="" name="username"/>
+		<input type="text" value="" name="username" required/>
 		<br/>
 		<label>Email Address: </label><br/>
-		<input type="email" value="" name="email"/>
+		<input type="email" value="" name="email" required/>
 		<br/>
-		<label>First Name: </label>
-		<input type="text" value="" name="firstname"/>
+		<label>First Name: </label><br/>
+		<input type="text" value="" name="firstname" required/>
 		<br/>
-		<label>Last Name: </label>
-		<input type="text" value="" name="lastname"/>
+		<label>Last Name: </label><br/>
+		<input type="text" value="" name="lastname"  required/>
 		<br/>
-		<label>Password: </label>
-		<input type="password" value="" name="pwd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
+		<label>Age: </label><br/>
+		<input type="number" value="" name="age" min="15" max="150" required/>
 		<br/>
-		<label>Confirm Password: </label>
-		<input type="password" value="" name="confpwd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
-		<p>NOTE: Password must contain at least: one capital and lowcase, number and special character.</p><br/>
-		<label>Age: </label>
-		<input type="number" value="" name="age" min="18" max="150"/>
+		<label>Password: </label><br/>
+		<input type="password" value="" name="pwd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required/>
+		<br/>
+		<label>Confirm Password: </label><br/>
+		<input type="password" value="" name="confpwd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required/>
+		<sub>NOTE: Password must contain at least: one capital and lowcase, number and special character.</sub><br/>
 		<br/>
 		<input type="submit" value="submit"/>
 		</form>
